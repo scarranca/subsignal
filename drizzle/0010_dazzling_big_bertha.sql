75dd522e059d51c85c44faf3a0c73f2a37e89751
wizenheimer <xNayanKumar@gmail.com>
2025-09-01 03:10:52 +0530

    fix: add additionals indexs for tables

---
+++ drizzle/0010_dazzling_big_bertha.sql
@@ -0,0 +1,3 @@
+CREATE INDEX "account_user_id_idx" ON "account" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "session_user_id_idx" ON "session" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "verification_identifier_idx" ON "verification" USING btree ("identifier");
